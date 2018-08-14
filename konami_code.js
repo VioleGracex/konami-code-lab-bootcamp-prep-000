a53643ee8818d3a3cc79e2c098d8852172590a25
Viole Grace <highdevilxx@hotmail.com>
2018-08-14 19:00:01 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -15,10 +15,11 @@ const code = [
  function init()
   {
    // your code here
-  
+
    var index = 0;
    document.querySelector('body').addEventListener('keydown', function (e) {
      //const key = parseInt(e.detail || e.which);
+     const key = e.key
 
     if (key === code[index])
      {
