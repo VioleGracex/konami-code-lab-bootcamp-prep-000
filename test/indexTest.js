41aaf1ed66ad0c19dcb0e2ca2955a438d15f710f
Viole Grace <highdevilxx@hotmail.com>
2018-08-14 17:03:07 +0000

    automatically backed up by learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,49 +1,20 @@
-const expect = chai.expect;
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-describe("index.js", () => {
+ function init() {
+   // your code here
+   var index = 0;
+   document.addEventListener('keydown', function (e) {
+     const key = parseInt(e.detail || e.which);
 
-  const codes = [
-    "ArrowUp",
-    "ArrowUp",
-    "ArrowDown",
-    "ArrowDown",
-    "ArrowLeft",
-    "ArrowRight",
-    "ArrowLeft",
-    "ArrowRight",
-    "b",
-    "a"
-  ];
+     if (key === code[index]) {
+       index++;
 
-  function triggerKeyDown(key) {
-    const keyboardEvent = new KeyboardEvent("keydown", { key });
-    document.body.dispatchEvent(keyboardEvent);
-  }
-  
-  init()
-
-  describe("Konami code", () => {
-    
-    const spy = sinon.stub(window, "alert");
-    
-    it("triggers an alert if the right code is entered", () => {
-      
-      for (let i = 0, l = codes.length; i < l; i++) {
-        triggerKeyDown(codes[i]);
-      }
-      expect(spy.called).to.equal(true)
-      expect(spy.callCount).to.equal(1)
-    });
-
-    it("does not trigger an alert if the wrong code is entered", () => {
-      spy.reset()
-
-      for (let i = 0; i < codes.length - 1; i++) {
-        triggerKeyDown(codes[i])
-      }
-      triggerKeyDown("ArrowUp")
-      
-      expect(spy.notCalled).to.equal(true);
-    });
-  });
-});
+       if (index === code.length - 1) {
+         window.alert("YOU DID IT!");
+         index = 0;
+       }
+     } else {
+       index = 0;
+     }
+   }, false);
+ }
